f0328cab4e2feb5899513b8d2661665a1fbffd5a
mrbonsoir <jeremie.gerhardt@gmail.com>
2015-01-29 11:06:08 +0100

    tiny stuffy

--- src/colorDifferences.py
+++ src/colorDifferences.py
@@ -1,24 +1,23 @@
 '''
 Created on 16.01.2013
-This thing should store all functions for computing color differences such as CIE Delta E and so...
-@author: gerjer
+This thing should store all functions for computing color differences such as
+CIE Delta 76, 94 and so...
+Jeremie Gerhardt
 '''
 
 import numpy as np
+import math
 
-def funDifferenceLab(Lab1, Lab2):
-    '''
-    Lab1 and lab2 are 3xn size
-    The output is 1xn size
-    '''
-    diffLab = np.sqrt(np.sum((Lab1-Lab2)*(Lab1-Lab2)))
-    return diffLab
+def fun_difference_CIELab76(Lab1, Lab2):
+    ''' Compute the Delta CIE 76 color difference between Lab1 and Lab2.
 
-#def funDifferenceL(L1, L2):
-	#'''
-    #L1 and L are 1xn size
-    #The output is 1xn size
-    #'''
-    #print L1, L2
-    #diffL = L1#diffL = np.sqrt(np.sum((L1-L2)*(L1-L2)))
-    #return diff
+    Args:
+        Lab1, Lab2 (float [floats]): two Lab values or 2 sets of Lab x np
+
+    Output:
+        difference_Lab (float [floats]): vector of size n.
+
+    '''
+    difference_Lab = np.sqrt(np.sum((Lab1-Lab2)*(Lab1-Lab2)))
+    
+    return difference_Lab
